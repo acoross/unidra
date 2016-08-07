f45cfc0e51669457d9763d1321d758d73faabb0c
acoross <acoross@gmail.com>
2016-08-07 20:35:29 +0900

    ch5 끝

---
+++ unidra/Assets/CameraFollowCharacter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowCharacter : MonoBehaviour {
+
+    public GameObject myFollowingCharacter;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = myFollowingCharacter.transform.position;
+        transform.Translate(new Vector3(0f, 2f, -3f));
+    }
+}
--- unidra/Assets/MoveCamera.cs
+++ unidra/Assets/MoveCamera.cs
@@ -17,6 +17,18 @@ public class MoveCamera : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
+        //if (Input.GetKey(KeyCode.LeftArrow)) {
+        //    transform.Translate(new Vector3(-1.0f, 0f, 0f));
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow)) {
+        //    transform.Translate(new Vector3(1.0f, 0f, 0f));
+        //}
+        //if (Input.GetKey(KeyCode.UpArrow)) {
+        //    transform.Translate(new Vector3(0.0f, 0f, 1f));
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow)) {
+        //    transform.Translate(new Vector3(0.0f, 0f, -1f));
+        //}
 
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
