4847b142cd784355d4a4591bcc13169a533f6ccb
acoross <acoross@gmail.com>
2016-08-07 19:43:17 +0900

    4장. 배경 추가함

---
+++ unidra/Assets/MoveCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+    float speedH = 3f;
+    float speedV = 3f;
+
+    float yaw = 0f;
+    float pitch = 0f;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+    }
+}
---
+++ unidra/Assets/MoveRigitObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRigitObject : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Vector3 force = new Vector3(-1.0f, 0f, 0f);
+            GetComponent<Rigidbody>().AddForce(force);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            Vector3 force = new Vector3(1.0f, 0f, 0f);
+            GetComponent<Rigidbody>().AddForce(force);
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            Vector3 force = new Vector3(0.0f, 0f, 100f);
+            GetComponent<Rigidbody>().AddForce(force);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            Vector3 force = new Vector3(0.0f, 0f, -1f);
+            GetComponent<Rigidbody>().AddForce(force);
+        }
+    }
+}
